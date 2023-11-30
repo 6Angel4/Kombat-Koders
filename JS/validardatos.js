ed911e012008b41cb6208681f9162023efc1d8a7
Mario Mata <mmatas1300@gmail.com>
2023-11-30 07:19:04 -0600

    El formulario ya no recibe letras en el campo de telefono, ahora llega un correo a petsupermarket.contato@gmail.com con la información del formulario

--- JS/validardatos.js
+++ JS/validardatos.js
@@ -2,28 +2,23 @@ const datosVerificados = (usuario) => {
     let esValido = true;
     const nombreRegex = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
     const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    const telefonoSinEspacios = usuario.telefono.replace(/[ -]/g, "");
-<<<<<<< HEAD
-    const telefonoRegex = /^(\d{2})?(\d{2})(\d{2})(\d{2})(\d{2})$/;
-=======
-    const telefonoRegex = /^(55)?(\d{2})(\d{2})(\d{2})(\d{2})$/;
->>>>>>> e03bfedf7f5f81fde88bbc6ebbb14ed41189dd08
-    
+    const telefonoRegex = /^[0-9]+$/;
+    const telefonoSinSeparacion = usuario.telefono.replace(/[ +-.,()]/g, "");
 
     if (usuario.nombre.trim().length === 0) {
-        alert("Falta tu nombre");
+        alert("Ingresa tu nombre, por favor.");
         esValido = false;
     } else if (!nombreRegex.test(usuario.nombre)) {
-        alert("Ingresa solo letras");
+        alert("Ingresa sólo letras, por favor.");
         esValido = false;
     } else if (!emailRegex.test(usuario.email)) {
-        alert("Este email no tiene un formato valido");
+        alert("Ingresa un correo electrónico con formato valido.");
         esValido = false;
-    } else if (!telefonoRegex.test(telefonoSinEspacios) || !telefonoSinEspacios.length === 10) {
-        alert("Tú número no es valido");
+    } else if (!(telefonoSinSeparacion.length === 10) || !(telefonoRegex.test(telefonoSinSeparacion))) {
+        alert("Ingresa un número valido, por favor.");
         esValido = false;
     } else if (usuario.mensaje === "") {
-        alert("Por favor escribe un mensaje");
+        alert("Escribe tu mensaje, por favor.");
         esValido = false; 
     }  else if (mensaje.length < 3) {
         alert("Tu mensaje es muy corto, para una buena comunicación te pedimos que nos cuentes con más detalles tu situación.");
@@ -32,30 +27,19 @@ const datosVerificados = (usuario) => {
     return esValido;
 }
 
-
-
 document.getElementById("formulario").addEventListener("submit", (event) => {
-    //getE... permite acceder y manipular elementos HTML por el atributo id("formulario"),utiliza addEventListener para escuchar el evento "submit"
-    event.preventDefault(); // Prevenir el envío por defecto del formulario
 
+    //getE... permite acceder y manipular elementos HTML por el atributo id("formulario"),utiliza addEventListener para escuchar el evento "submit"
 const usuario = { 
-    
     nombre : document.getElementById('nombre').value,
     email : document.getElementById("correo").value, //obtiene el valor de un campo de HTML con el id "correo, se accede al elemento mediante document.getElementById('correo'), y luego se extrae su valor con .value
     telefono : document.getElementById("telefono").value,
     mensaje : document.getElementById("mensaje").value
     }   
 
-    if( datosVerificados( usuario ) ){
-//         enviarDatosAlServidor( usuario );
-  }
+    if(!datosVerificados(usuario)) event.preventDefault(); // Prevenir el envío por defecto del formulario
 
- });
-
-
-
-
-
+});
 
 ///^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
@@ -77,36 +61,4 @@ const usuario = {
 // )+               El grupo anterior debe aparecer una o más veces
 // $                Coincide con el final de la cadena
 
-// document.getElementById('contactForm').addEventListener('submit', function(event) {
-//     event.preventDefault(); // Prevenir el envío por defecto del formulario
 
-//     // Obtener los valores de los campos
-//     const name = document.getElementById('name').value;
-//     const email = document.getElementById('email').value;
-//     const phone = document.getElementById('phone').value;
-//     const message = document.getElementById('message').value;
-
-//     // Validaciones básicas
-//     if (name.trim() === '' || email.trim() === '' || message.trim() === '') {
-//         alert('Por favor, complete todos los campos obligatorios.');
-//         return;
-//     }
-
-//     // Validar el formato del correo electrónico
-// const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-// if (!emailRegex.test(email)) {
-//     alert('Por favor, ingrese un correo electrónico válido.');
-//     return;
-// }
-
-// // simulación
-// console.log('Datos enviados (simulación):');
-// console.log('Nombre:', name);
-// console.log('Correo electrónico:', email);
-// console.log('Teléfono:', phone);
-// console.log('Mensaje:', message);
-
-// alert('¡Mensaje enviado con éxito!');
-// });
-
-//  Obtener los valores de phone
