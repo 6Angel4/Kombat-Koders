445080c6b460cc6341a5c2565ea7e17286027109
Alejandro Flores <alejandro.floesc@gmail.com>
2023-12-04 23:12:55 -0600

    Funcion agregar productos - Ejemplo repositorio

--- JS/listproducts.js
+++ JS/listproducts.js
@@ -1,27 +1,45 @@
-
 // Función que recibe los datos que provienen del formulario en HTML
-function guardarProducto () {
 
-    const id = document.getElementById("id").value;
-    const nombreProducto = document.getElementById("nombreProducto").value;
-    const contenidoProducto = document.getElementById("contenidoProducto").value;
-    const precio = document.getElementById("Precio").value;
-    const marca = document.getElementById("marca").value;
-    const tipoProducto = document.getElementById("tipoProducto").value;
-    
-    const producto = {
-        id: id,
-        nombreProducto: nombreProducto,
-        contenidoProducto: contenidoProducto,
-        precio: precio,
-        marca: marca,
-        productoPara: productoPara,
-        tipoProducto: tipoProducto
-    };
+const agregarProducto = () => {
+  const nombreProducto = document.getElementById("nombreProducto").value;
+  const contenidoProducto = document.getElementById("contenidoProducto").value;
+  const precio = document.getElementById("precio").value;
+  const marca = document.getElementById("marca").value;
+  const tipoProducto = document.getElementById("tipoProducto").value;
 
-    //const productoJSON = JSON.stringify(producto);
-    console.log("Producto(JSON):");
+  producto.agregarProducto(
+    nombreProducto,
+    contenidoProducto,
+    precio,
+    marca,
+    tipoProducto
+  );
+
+  console.log(producto.productoJSON());
 };
 
+// Agregar productos - Ejemplo Respositorio
 
+// class ControlProducto {
+//     constructor(productoId=0){
+//         this.producto = [];
+//         this.productoId = productoId;
+//     }
 
+//     agregarProducto(nombreProducto, contenidoProducto, precio, marca, tipoProducto) {
+//         const articulo = {
+
+//             id: this.productoId++,
+//             nombreProducto: nombreProducto,
+//             contenidoProducto: contenidoProducto,
+//             precio: precio,
+//             marca: marca,
+//             tipoProducto: tipoProducto
+//         };
+
+//         this.producto.push(articulo);
+//     }
+//         productoJSON(){
+//             return JSON.stringify(this.producto)
+//         }
+// }
