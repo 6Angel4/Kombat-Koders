4e026a5263472a0792fe6a3c1c8679d29c480357
Cynthia Rosales <cynthiarosalesguevara@gmail.com>
2023-12-06 18:28:41 -0600

    eliminar todos los productos

--- JS/listproducts.js
+++ JS/listproducts.js
@@ -1,37 +1,10 @@
-let listaProductosQueSeVaAMostrarEnLaPagina = [];
 
-function cargaPagina() {// ejemplo
-    /*
-    imaginemos que aqui hicimos todo el proceso para cargar la pagina
-    cargar los html/css/js, hacer los gets, etc
-    */
-   const datosDeProductosQueLLegaronDelBackend = [
-    {
-       nombre:"obj 1" ,
-       datos: "otros datos"
-    },
-    {
-        nombre:"obj 2" ,
-        datos: "otros datos"
-     },
-     {
-        nombre:"obj 3" ,
-        datos: "otros datos"
-     },
-     {
-        nombre:"obj 4" ,
-        datos: "otros datos"
-     }
-   ]
-    listaProductosQueSeVaAMostrarEnLaPagina = datosDeProductosQueLLegaronDelBackend;
+
+function borrarTodosElementos() {
+   //llamar funcion que reformatee el html sin productos para vender
+   
+   localStorage.removeItem("producto", []);
+   //localStorage.clear();// afecta todos los datos
 }
 
-function borrarTodosElementos() {//llamar funcion que reformatee el html sin productos para vender
-    
-    listaProductosQueSeVaAMostrarEnLaPagina = [];
-}
-
-cargaPagina()
-console.log("quedan "+listaProductosQueSeVaAMostrarEnLaPagina.length, " productos totales");//quedan 4 elementos
-borrarTodosElementos();//funcion que eliminara todos los elementos, 
-console.log("quedan "+listaProductosQueSeVaAMostrarEnLaPagina.length, " productos totales");//quenda 0 elementos
+borrarTodosElementos();//llamada a funcion que eliminara todos los elementos, 
