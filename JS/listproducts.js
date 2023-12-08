6db5b95c81b2e32c80976ed6bd0828b7c219fdd0
Alejandro Flores <alejandro.floesc@gmail.com>
2023-12-07 18:27:45 -0600

    Se agrego DOM

--- JS/listproducts.js
+++ JS/listproducts.js
@@ -1,63 +1,63 @@
+const urlProductos ="../productos.json";
 
 
-function borrarTodosElementos() {
-   //llamar funcion que reformatee el html sin productos para vender
-   
-   localStorage.setItem("producto", []);
-   //localStorage.clear();// afecta todos los datos
+const getProductos = (url) => {
+fetch(url)
+.then((response) => {
+  console.log("status code: " + response.status); // 200
+  return response.json();
+})
+.then( productos => {
+  // console.log(products); 
+  imprimirEnDOM( productos );
+} )
+.catch((error) => {
+  console.log("Error en la solicitud: ");
+  console.warn(error);
+});
 }
 
-borrarTodosElementos();//llamada a funcion que eliminara todos los elementos, 
+
+const getProductosUsandoAsyncAwait = async ( url ) => {
+  try {
+     const response = await fetch(url);
+     const productos = await response.json();
+     imprimirEnDOM( productos );   
+     
+  } catch (error) {
+     console.log( error );
+  }
+
+}
+
+getProductosUsandoAsyncAwait(urlProductos);
+
+function imprimirEnDOM( productos ) {
+    const contenedorProductos = document.getElementById("productos-contenedor");
+
+    const articulos = productos.map( (producto, index, array)=> `
+        
+          <div class="d-flex justify-content-center col-sm-12 col-md-6 col-lg-3">
+            <div class="card border-0 mb-5"><!--Aqui esta la primera tarjeta de producto-->
+              <img src="${producto.imagen}" class="card-img-top" alt="..." />
+              <div class="card-body text-center d-flex align-items-center justify-content-between flex-column">
+                <div>
+                  <h5 class="card-title">${producto.nombreProducto}</h5>
+                  <p class="card-text">${producto.Descripcion}</p>
+                  <p>$${producto.precio}</p>
+                </div>
+                <a href="#" class="btn btn-primary">Añadir a carrito</a>
+              </div>
+            </div> 
+          </div>         
+   `     
+    );    
+   
+    console.log( articulos );
+    contenedorProductos.innerHTML =  articulos.join("");
+   
+ }
 
 
-// function borrarTodosElementos(productos) {
-//    productos =[];
 
-// }
 
-// borrarTodosElementos();//llamada a funcion que eliminara todos los elementos, 
-// Función que recibe los datos que provienen del formulario en HTML
-
-const agregarProducto = () => {
-  const nombreProducto = document.getElementById("nombreProducto").value;
-  const contenidoProducto = document.getElementById("contenidoProducto").value;
-  const precio = document.getElementById("precio").value;
-  const marca = document.getElementById("marca").value;
-  const tipoProducto = document.getElementById("tipoProducto").value;
-
-  producto.agregarProducto(
-    nombreProducto,
-    contenidoProducto,
-    precio,
-    marca,
-    tipoProducto
-  );
-
-  console.log(producto.productoJSON());
-};
-
-// Agregar productos - Ejemplo Respositorio
-
-// class ControlProducto {
-//     constructor(productoId=0){
-//         this.producto = [];
-//         this.productoId = productoId;
-//     }
-
-//     agregarProducto(nombreProducto, contenidoProducto, precio, marca, tipoProducto) {
-//         const articulo = {
-
-//             id: this.productoId++,
-//             nombreProducto: nombreProducto,
-//             contenidoProducto: contenidoProducto,
-//             precio: precio,
-//             marca: marca,
-//             tipoProducto: tipoProducto
-//         };
-
-//         this.producto.push(articulo);
-//     }
-//         productoJSON(){
-//             return JSON.stringify(this.producto)
-//         }
-// }
