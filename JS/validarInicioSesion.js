ba3351901b812d7d46e7179ae20664380820fbfe
Alejandro Flores <alejandro.floesc@gmail.com>
2023-12-18 23:45:56 -0600

    Alertas corregidas

--- JS/validarInicioSesion.js
+++ JS/validarInicioSesion.js
@@ -2,94 +2,57 @@ import { ControladorRegistro } from "./controladorRegistro.js";
 
 document.getElementById("formulario-inicio-sesion").addEventListener("submit", (event) => {
 
-    event.preventDefault();
-    document.getElementById("inicio-sesion-exitoso").style.display="none";    
-    document.getElementById("completa-campos").style.display="none";
-    document.getElementById("no-registrado").style.display="none";
-    document.getElementById("error-login-email").style.display="none";
-    document.getElementById("error-login-password").style.display="none";
+  event.preventDefault();
+  document.getElementById("inicio-sesion-exitoso").style.display = "none";
+  document.getElementById("completa-campos").style.display = "none";
+  document.getElementById("no-registrado").style.display = "none";
+  document.getElementById("error-login-email").style.display = "none";
+  document.getElementById("error-login-password").style.display = "none";
 
-    
-    // Obtener los valores de email y contraseña
-    const email = document.getElementById("loginEmail").value;
-    const password = document.getElementById("loginPassword").value;
- 
-
-
-    // Expresiones regulares
-    let esValido = true; 
-    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  // Obtener los valores de email y contraseña
+  const email = document.getElementById("loginEmail").value;
+  const password = document.getElementById("loginPassword").value;
+  const usuarioActual = {
+    username: email,
+    password: password
+  }
   
-    // Validar campos vacíos
-    if (email.trim() === "" || password.trim() === "") {
-    document.getElementById("completa-campos").style.display="block";   
-    esValido = false;
-    } else if (!emailRegex.test(email)) {
-      document.getElementById("error-login-email").style.display="block";
-      esValido = false;
-    } else if (!passwordRegex.test(password)) {
-        document.getElementById("error-login-password").style.display="block";
-        esValido = false;
-    }  
-    const usuarioActual = {
-      username: email,
-      password: password
-    }; 
-  
-    // Controlador
+  // Verificar datos de usuario
+  if (verificacarInicioSesion(usuarioActual)) {
     const miControladorRegistro = new ControladorRegistro();
-    // Verificar datos de usuario
     const usuarioRegistrado = miControladorRegistro.buscarRegistroLocalStorage(usuarioActual.username, usuarioActual.password);
   
-    if (usuarioRegistrado) {
+  if (usuarioRegistrado) {
       document.getElementById("inicio-sesion-exitoso").style.display = "block";
       localStorage.setItem("usuarioActual", JSON.stringify(usuarioActual));
-  } else {
-      // Ocultar mensajes previos
+      document.getElementById("loginEmail").value = "";
+      document.getElementById("loginPassword").value ="";
+    } else {      
       document.getElementById("no-registrado").style.display = "block";
-      document.getElementById("error-login-password").style.display = "none";
-      document.getElementById("completa-campos").style.display = "none";
-  
-      // Mostrar mensajes específicos
-      if (email.trim() === "") {
-          // El campo de correo está incompleto
-          document.getElementById("completa-campos").style.display = "block";
-      } else if (!emailRegex.test(email)) {
-          // El campo de correo no es válido
-          document.getElementById("error-login-email").style.display = "block";
-      } else if (!miControladorRegistro.existeUsuarioLocalStorage(usuarioActual.username)) {
-          // El usuario no está registrado
-          document.getElementById("no-registrado").style.display = "block";
-      } else {
-          // Contraseña incorrecta
-          document.getElementById("error-login-password").style.display = "block";
-      }
-  }
-
-
+  }  
+}
 });
 
+// Validaciones
+const verificacarInicioSesion = (usuario) => {
+  // Expresiones regulares
+  let esValido = true;
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
-
-
-
-
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // Validar campos vacíos
+  if (usuario.username.trim() === "" && usuario.password.trim() === "") {
+    document.getElementById("completa-campos").style.display = "block";
+    esValido = false;
+  } else if (!emailRegex.test(usuario.username)) {
+    document.getElementById("error-login-email").style.display = "block";
+    esValido = false;
+  } else if (!passwordRegex.test(usuario.password)) {
+    document.getElementById("error-login-password").style.display = "block";
+    esValido = false;
+  }
+  return esValido;
+}
 
 
 
@@ -136,8 +99,8 @@ document.getElementById("formulario-inicio-sesion").addEventListener("submit", (
 
 // // Obtener valores del formmulario
 // const inicioSesion = {
-//     email: document.getElementById("loginEmail").value,         
-//     password : document.getElementById("loginPassword").value       
+//     email: document.getElementById("loginEmail").value,
+//     password : document.getElementById("loginPassword").value
 // }
 
 // // Verificar datos del formulario son validos antes de enviar
@@ -147,8 +110,8 @@ document.getElementById("formulario-inicio-sesion").addEventListener("submit", (
 //         inicioSesion.email,
 //         inicioSesion.contrasena);
 
-// // Limpiar los campos del formmulario  
-//     document.getElementById("loginEmail").value = "";         
+// // Limpiar los campos del formmulario
+//     document.getElementById("loginEmail").value = "";
 //     document.getElementById("loginPassword").value = "";
 
 //     setTimeout(function() {
